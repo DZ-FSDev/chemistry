5ff15d38a0303c82fc81fd43636e147f56b13482
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-11-19 10:24:51 -0600

    Create ElementProperties.java

---
+++ cli/ElementProperties.java
@@ -0,0 +1,32 @@
+package com.dz_fs_dev.chemistry.cli;
+
+import org.openscience.cdk.config.Elements;
+
+/**
+ * Command-line display of atomic properties given an element.
+ *
+ * @author DZ-FSDev
+ * @since 16.0.1
+ * @version 0.0.1
+ */
+public class ElementProperties {
+	public static void main(String[] args) {
+		if(args.length > 0) {
+			Elements element = Elements.valueOf(args[0]);
+			System.out.println(element.name());
+			System.out.println("Group:\t" + element.group());
+			System.out.println("Atomic Number:\t" + element.number());
+			System.out.println("Period:\t" + element.period());
+			System.out.println("Symbol:\t" + element.symbol());
+			System.out.println("Covalent Radius:\t" + element.covalentRadius());
+			System.out.println("Electronegativity:\t" + element.electronegativity());
+			System.out.println("VDW Radius:\t" + element.vdwRadius());
+			System.out.println("Metal:\t" + Elements.isMetal(element.number()));
+		}else if(args.length == 1 && args[0].equals("/?")){
+			System.out.println("Expected argument list: element");
+			System.out.println();
+		}else {
+			System.out.println("Invalid arguments were provided. Use /? for more information.");
+		}
+	}
+}
