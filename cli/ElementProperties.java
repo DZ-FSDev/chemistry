34ef5751df4025fc206200d6c49e0e7be281920b
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-11-28 10:25:49 -0600

    Update ElementProperties.java

--- cli/ElementProperties.java
+++ cli/ElementProperties.java
@@ -11,7 +11,7 @@ import org.openscience.cdk.interfaces.IIsotope;
  *
  * @author DZ-FSDev
  * @since 17.0.1
- * @version 0.0.4
+ * @version 0.0.5
  */
 public class ElementProperties {
 	public static void main(String[] args) {
@@ -30,14 +30,16 @@ public class ElementProperties {
 			IIsotope isotope;
 			try {
 				isotope = Isotopes.getInstance().getMajorIsotope(element.symbol());
+				System.out.println("Isotope:\t" + element.symbol() + isotope.getMassNumber());
 				System.out.println("Natural Abundance:\t" + isotope.getNaturalAbundance());
 				System.out.println("Mass:\t" + isotope.getExactMass());
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println("Isotope Data Not Available");
+				//e.printStackTrace();
 			}
 			
 		}else if(args.length == 1 && args[0].equals("/?")){
-			System.out.println("Expected argument list: element");
+			System.out.println("Expected argument list: element [mass number]"); //TODO
 			System.out.println();
 		}else {
 			System.out.println("Invalid arguments were provided. Use /? for more information.");
